284bb6ee3670b71ab25513aceba992247c02b811
zeilerphone <zeiler@ucsb.edu>
2024-09-19 18:43:48 -0700

    final changes to cnf to mem conversion python code. this should allow conversion of any cnf file to the .mem files needed by the hardware solver

--- Python-Code/packing_new.py
+++ Python-Code/packing_new.py
@@ -1,5 +1,11 @@
 import parsecnf as rd
-import os
+import random
+
+def clog2(x):
+    """Ceiling of log2"""
+    if x <= 0:
+        raise ValueError("domain error")
+    return (x-1).bit_length()
 
 def convert_list_to_binary_string(mylist, bits_per_element):
     """
@@ -18,20 +24,28 @@ def convert_list_to_binary_string(mylist, bits_per_element):
 
 def create_clause_list(filename):
     """
-    Creates a list of clauses based on a given CNF file.
+    Creates a list of clauses based on a given CNF file in the format needed for the hardware solver.
+    (L1 is 0, ~L1 is num_vars, L2 is 1, ~L2 is num_vars + 1, etc.)
 
     Args:
     filename (string): A problem .cnf file
 
     Returns:
-    num_vars (int): The number of variables in the CNF file
-    clause_list (list): A list of clauses, each clause being a list of literals
+    fixed_num_vars (int): The number of variables in the CNF file
+    fixed_clause_list (list): A list of clauses, each clause being a list of literals
     """
     num_vars, clauses = rd.read_dimacs(filename)
     clause_list = []
     for clause in clauses:
         clause_list.append(clause[:3])
-    return num_vars, clause_list
+    fixed_clause_list = []
+
+    fixed_num_vars = 2 ** clog2(num_vars)
+
+    for clause in clause_list:
+        fixed_clause = [(_-1 if _>0 else abs(_)-1 + fixed_num_vars) for _ in clause]
+        fixed_clause_list.append(fixed_clause)
+    return fixed_num_vars, fixed_clause_list    
 
 def generate_clause_membership_list(num_vars, clause_list, is_exclusive = True):
     """
@@ -56,19 +70,35 @@ def generate_clause_membership_list(num_vars, clause_list, is_exclusive = True):
     for clause in clause_list:
         for literal in clause:
             if(is_exclusive):
-                fixed_clause = [(_-1 if _>0 else abs(_)-1 + num_vars) for _ in clause if _ != literal]
+                use_clause = [_ for _ in clause if _ != literal]
             else:
-                fixed_clause = [(_-1 if _>0 else abs(_)-1 + num_vars) for _ in clause]
-            
-            if literal > 0:
-                literal_clause_membership_list[literal - 1][1].append(fixed_clause)
-            else: # negated literal
-                literal_clause_membership_list[abs(literal) - 1 + num_vars][1].append(fixed_clause)
+                use_clause = [_ for _ in clause]
+            literal_clause_membership_list[literal][1].append(use_clause)
     return literal_clause_membership_list
 
 
 def generate_att_and_ct(literal_clause_membership_list):
-    num_literals = len(literal_clause_membership_list)
+    """
+    Generates the Address Translation Table and Clause Table based on the literal clause membership 
+    list by running a modified version of the packing algorithm in packing.py 
+
+    Args:
+    literal_clause_membership_list (list): A list of literals and membership, each element being a
+    list of two elements: the literal and a list of the literals in the clauses that literal is in
+
+    Returns:
+    att (list): The Address Translation Table entries which are 31 bits wide. Indices under num_vars
+    represent the un-negated literals, and indices over num_vars represent the negated literals.
+    The first 11 bits are  the index of the clause table entry for the literal, and the next 20 bits
+    are the mask for the literal.
+
+    ct (list): The Clause Table entries which are 12*2*20 bits wide. Each entry is a list of 20 
+    groups of 2 literals corresonding to the other two literals in each of the clauses that the  
+    literal used to index the att is in.
+
+    The returns are lists in the format necessary for the .mem files for the hardware solver.
+    """
+    # num_literals = len(literal_clause_membership_list)
     literal_membership_count = [len(literal[1]) for literal in literal_clause_membership_list]
     literal_membership_count_sorted = sorted(enumerate(literal_membership_count), key=lambda x: x[1])
 
@@ -108,10 +138,54 @@ def generate_att_and_ct(literal_clause_membership_list):
 
     return att, ct
 
+def generate_vt_and_ucb(variable_table_size, clauses, num_tries):
+    """
+    Generates num_tries random assignments for the variable table and an unsat clause buffers based
+    on that assignment. It returns the variable table and unsat clause buffer that have the minimum 
+    number of unsatisfied clauses
+
+    Args:
+    variable_table_size (int): The number of variables in the problem (power of 2)
+    clauses (list): A list of clauses, each clause being a list of literals
+    num_tries (int): The number of random assignments to try
+    Returns:
+    vt (list): The variable table with random assignments
+    ucb (list): A list of unsatisfied clauses for the generated variable assignment
+    """
+
+    best_vt = [0 for _ in range(variable_table_size)]
+    best_ucb = [0 for _ in range(len(clauses))]
+    
+    for _ in range(num_tries):
+        candidate_vt = [random.randint(0, 1) for _ in range(variable_table_size)]
+        candidate_ucb = []
+        for clause in clauses:
+            is_sat = False
+            for literal in clause:
+                if literal < variable_table_size:
+                    if candidate_vt[literal] == 1:
+                        is_sat = True
+                        break
+                else: # negated literal
+                    if candidate_vt[literal - variable_table_size] == 0:
+                        is_sat = True
+                        break
+            if not is_sat:
+                candidate_ucb.append(clause)
+
+        if len(candidate_ucb) < len(best_ucb) or not best_ucb:
+            best_vt = candidate_vt.copy()
+            best_ucb = candidate_ucb.copy()
+    
+    ucb = [convert_list_to_binary_string(entry, 12) for entry in best_ucb]
+
+
+    return best_vt, ucb
+
 def write_list_to_file(mylist, filename):
     with open(filename, 'w') as file:
         for element in mylist:
-            file.write(element + '\n')            
+            file.write(str(element) + '\n')            
 
 def write_to_memb_file(uint32_list, filename, bits_per_line=32):
     """
